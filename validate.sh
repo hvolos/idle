e7a9002551399eed0dce648d8b196666fdff58c2
hvolos01 <hvolos01@localhost>
2022-03-10 13:01:22 -0600

    use printf only at idle probe

--- validate.sh
+++ validate.sh
@@ -3,4 +3,5 @@
 rm -f validation_idle.txt
 sudo stap idle.stp -o out -c './activate.sh; ./idle_test 10 25 25000 10'
 cat validation_idle.txt
-python3 idle.py out `awk '$1 ~ /^begin_ts/ {print $2}' out` `awk '$1 ~ /^end_ts/ {print $2}' out`
+sort -s -n -k 1,1 out > out2
+python3 idle.py out2 `awk '$1 ~ /^begin_ts/ {print $2}' out2` `awk '$1 ~ /^end_ts/ {print $2}' out2`
